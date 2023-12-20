48bfbc1e46e9d7777bd1bc465238c2bc3867c6a1
yangjiefeng <yangjiefeng@gmail.com>
2023-12-20 19:28:05 +0800

    update README

--- sui-contracts/create_slots.sh
+++ sui-contracts/create_slots.sh
@@ -3,8 +3,8 @@
 package_id=0x776fbac3115141986702630edf576b42adbcf60dc86a56a5875703fe817b670e
 slot_number_table_id=0x19d0c8a74789724f7df600c1089a16f58f3222a20606865acf115faaa9381263
 
-slot_number=2
-while [ $slot_number -lt 209 ]
+slot_number=180
+while [ $slot_number -lt 181 ]
 do
 sui client call --package $package_id --module slot_aggregate --function create \
 --args $slot_number 0x6 \"$slot_number_table_id\" \
---
+++ sui-contracts/do_first_round.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#txn_digest=$(cat test_sui_publish_package.json | jq -r '.digest')
+package_id="0x776fbac3115141986702630edf576b42adbcf60dc86a56a5875703fe817b670e"
+slot_number_table_id="0x19d0c8a74789724f7df600c1089a16f58f3222a20606865acf115faaa9381263"
+
+while IFS=, read -r col1 col2
+do
+slot_number=${col1//\'/}
+slot_object_id=${col2//\'/}
+echo "slot_number: $slot_number"
+echo "slot_object_id: $slot_object_id"
+round=0
+echo "current round: $round"
+
+mint_count=0
+while [ $mint_count -lt 1 ]
+do
+echo "current mint count: $mint_count"
+
+sui client call --package $package_id --module inscription_aggregate --function mint \
+--args $slot_number \"$round\" \"385802469\" \"3\" '"dddappp"' \"0x6\" \
+--gas-budget 1000000000 --json > test_minted_inscription.json
+
+minted_inscription_id=$(cat test_minted_inscription.json | jq -r '.objectChanges[] | select(.type == "created") | select(.objectType | test("::inscription::Inscription")).objectId')
+echo "minted_inscription_id: $minted_inscription_id"
+
+if [ -z "$minted_inscription_id" ]
+then
+continue
+fi
+
+sui client call --package $package_id --module slot_aggregate --function put_up_candidate \
+--args $slot_object_id \"$minted_inscription_id\" \"0x6\" \
+--gas-budget 1000000000
+
+mint_count=$((mint_count+1))
+done
+
+#sleep 150
+sui client call --package $package_id --module slot_aggregate --function advance \
+--args $slot_object_id \"0x6\" \
+--gas-budget 1000000000
+
+round=$((round+1))
+
+done < slots.txt
