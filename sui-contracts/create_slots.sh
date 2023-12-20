78c77c108e4d5fb5594ade4f3f052c9bf0f82f4b
yangjiefeng <yangjiefeng@gmail.com>
2023-12-20 17:00:01 +0800

    update README and config

--- sui-contracts/create_slots.sh
+++ sui-contracts/create_slots.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-package_id=0x51095498d7bd253ffaa719b20ece8efce3dc02f9ed6b65a9ce3072b43b2dd7b3
-slot_number_table_id=0xb38de3ca7eb3323857676622be013dcb7d6e67f4f36dc4383c867f7dce4675a8
+package_id=0x776fbac3115141986702630edf576b42adbcf60dc86a56a5875703fe817b670e
+slot_number_table_id=0x19d0c8a74789724f7df600c1089a16f58f3222a20606865acf115faaa9381263
 
-slot_number=0
-while [ $slot_number -lt 210 ]
+slot_number=2
+while [ $slot_number -lt 209 ]
 do
 sui client call --package $package_id --module slot_aggregate --function create \
 --args $slot_number 0x6 \"$slot_number_table_id\" \
 --gas-budget 1000000000
 
+echo "Slot object created, slot_number: $slot_number"
 slot_number=$((slot_number+1))
 done
