62120984de95791afd598d4ffdb7bb04bd67990a
yangjiefeng <yangjiefeng@gmail.com>
2023-12-20 14:27:12 +0800

    update shell

--- sui-contracts/test.sh
+++ sui-contracts/test.sh
@@ -8,7 +8,7 @@ echo "txn_digest: $txn_digest"
 package_id=$(cat test_sui_publish_package.json | jq -r '.objectChanges[] | select(.type == "published").packageId')
 echo "package_id: $package_id"
 
-slot_number_table_id=$(cat test_sui_publish_package.json | jq -r '.events[] | select(.transactionModule == "slot").parsedJson.id')
+slot_number_table_id=$(cat test_sui_publish_package.json | jq -r '.events[] | select(.type | test("::slot::SlotNumberTableCreated"))'.parsedJson.id)
 echo "slot_number_table_id: $slot_number_table_id"
 
 slot_number=0
@@ -22,20 +22,22 @@ echo "slot_object_id: $slot_object_id"
 round=0
 while [ $round -lt 1 ]
 do
+echo "current round: $round"
 
 mint_count=0
 while [ $mint_count -lt 5 ]
 do
+echo "current mint count: $mint_count"
 
 sui client call --package $package_id --module inscription_aggregate --function mint \
 --args $slot_number \"$round\" \"11111111\" \"11111111\" '"hello-world!"' \"0x6\" \
 --gas-budget 1000000000 --json > test_minted_inscription.json
 
-minted_inscription=$(cat test_minted_inscription.json | jq -r '.objectChanges[] | select(.type == "created") | select(.objectType | test("::inscription::Inscription")).objectId')
-echo "minted_inscription: $minted_inscription"
+minted_inscription_id=$(cat test_minted_inscription.json | jq -r '.objectChanges[] | select(.type == "created") | select(.objectType | test("::inscription::Inscription")).objectId')
+echo "minted_inscription_id: $minted_inscription_id"
 
 sui client call --package $package_id --module slot_aggregate --function put_up_candidate \
---args $slot_object_id \"$minted_inscription\" \"0x6\" \
+--args $slot_object_id \"$minted_inscription_id\" \"0x6\" \
 --gas-budget 1000000000
 
 mint_count=$((mint_count+1))
